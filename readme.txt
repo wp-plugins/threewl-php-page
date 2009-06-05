c09fa983a73d6a224c33ee7ea5ead7bdb90b4a74
videomike <videomike@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-05 16:17:05 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/threewl-php-page/trunk@123132 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== threewl-php-page ===
+Contributors: videomike
+Donate link: http://www.seo-traffic-guide.de/3wl-php-page-plugin/
+Tags: search engine optimization, search engine, SEO, link building, automatic link building, 3waylinks.net, SEO plugin
+Requires at least: 2.6
+Tested up to: 2.7.1
+Stable tag: trunk
+
+This plugin helps you integrate your wordpress site in the [3waylinks.net](http://www.seo-traffic-guide.de/recommends/3waylinks "3waylinks.net") backlink buidling system
+
+== Description ==
+
+[3waylinks.net](http://www.seo-traffic-guide.de/recommends/3waylinks "3waylinks.net") by Jon Leger 
+is an excellent link building system - there is no easier way to build "one way" backlinks - only that up to now 
+getting it running with wordpress could sometimes be a little bit difficult… This plugin makes the process of putting 
+a new wordpress site in the 3Waylinks system a lot easier.
+
+== Installation ==
+
+1. unzip threewl-php-page.zip to the `/wp-content/plugins/` directory and activate the plugin
+2. Follow the installation instructions on the [plugin homepage] (http://www.seo-traffic-guide.de/3waylinks-installation-instructions/ "Installation instructions")
+ 
+This is how it works in short words...
+After enabling this plugin, go to the plugin options page and create a static page 
+that can display the 3WL links. You can use the "Create Page" button on the plugin
+options page to do it for you.
+If you want to add the code to an existing page, embed this comment:
+
+  <!-- threewl_php_page -->
+  
+When the page is displayed, the 3WL PHP code is generated and
+your 3WL Links are displayed in place of the comment.
+
+== Frequently Asked Questions ==
+
+= I installed the plugin, now how do I get backlinks? =
+
+You need to be a member of the [3waylinks.net](http://www.seo-traffic-guide.de/recommends/3waylinks "3waylinks.net") network in order to receive links 
+
+= Where can I find my 3WL ID for an existing site? =
+
+Log in to your 3WL backend, click on "my domains" then click on edit behind the domain you need the ID for.
+Now if you have a look on the URL line in your browser it should look like http://3waylinks.net/index.php?action=domain&id=XXXXX
+Where XXXXX is your 3WL ID for that particular domain
+
+== Screenshots ==
+
---
+++ threewl-php-page.php
@@ -0,0 +1,249 @@
+<?php
+/*
+Plugin Name: threewl-php-page
+Plugin URI: http://www.seo-traffic-guide.de/3WL-PHP-Page-Plugin/
+Description: Create a page that contains the <a href="http://www.seo-traffic-guide.de/recommends/3waylinks">3waylinks.net</a> outgoing links for the 3waylinks linking system by Jon Leger <a href="options-general.php?page=threewl-php-page.php">Options configuration panel</a> This plugin is based on the Privacy Page Plugin by Eric Giguere (http://www.synclastic.com/plugins/privacy-policy/)
+Version: 0.3
+Author: Michael Busch
+Author URI: http://www.seo-traffic-guide.de
+*/
+ 
+/*
+To install:
+ 
+1. Upload threewl-php-page.zip to the /wp-content/plugins/ directory for your blog.
+2. Unzip it into /wp-content/plugins/threewl-php-page/threewl-php-page.php
+3. Activate the plugin through the 'Plugins' menu in WordPress by clicking "3WL Links Page"
+4. Go to your Options Panel and open the "3WL Links Page" submenu. /wp-admin/options-general.php?page=threewl-php-page.php
+5. Configure the 3waylinks options you want.
+
+License:
+
+Copyright 2008 by Michael Busch. You are free to use this plugin on 
+any WordPress blog. No warranty is provided -- not even that this plugin does what it is intended for
+*/
+
+$threewlphppage = '0.3';
+
+$pp_default_threewlid = 'your 3WL site ID';
+$pp_default_title = 'Resources';
+$pp_default_slug = 'resources';
+$pp_default_pp_help = true;
+$pp_default_credit = true;
+
+add_option( 'threewl_php_page_threewlid', $pp_default_threewlid );
+add_option( 'threewl_php_page_title', $pp_default_title );
+add_option( 'threewl_php_page_slug', $pp_default_slug );
+add_option( 'threewl_php_page_pp_help', $pp_default_pp_help );
+add_option( 'threewl_php_page_credit', $pp_default_credit );
+
+function threewl_php_page_options_setup() {
+    if( function_exists( 'add_options_page' ) ){
+        add_options_page( '3WL Links Page', '3WL Links Page', 8, 
+                          basename(__FILE__), 'threewl_php_page_options_page');
+    }
+
+}
+
+function threewl_php_page_options_page(){
+    global $threewl_php_page_ver;
+    global $pp_default_threewlid;
+    global $pp_default_title;
+    global $pp_default_slug;
+    global $pp_default_pp_help;
+    global $pp_default_credit;
+
+    if( isset( $_POST[ 'set_defaults' ] ) ){
+
+        echo '<div id="message" class="updated fade"><p><strong>';
+
+	update_option( 'threewl_php_page_title', $pp_default_title );
+	update_option( 'threewl_php_page_threewlid', $pp_default_threewlid );
+	update_option( 'threewl_php_page_slug', $pp_default_slug );
+	update_option( 'threewl_php_page_pp_help', $pp_default_pp_help );
+	update_option( 'threewl_php_page_credit', $pp_default_credit );
+
+	echo 'Default 3WL Links Page options loaded!';
+	echo '</strong></p></div>';
+
+    } else if( isset( $_POST[ 'create_page' ] ) ){
+
+        echo '<div id="message" class="updated fade"><p><strong>';
+
+	$title = stripslashes( (string) $_POST[ 'threewl_php_page_title' ] );
+	$slug  = stripslashes( (string) $_POST[ 'threewl_php_page_slug' ] );
+	$threewlid = stripslashes( (string) $_POST['threewl_php_page_threewlid' ] );
+
+	update_option( 'threewl_php_page_title', $title );
+	update_option( 'threewl_php_page_threewlid', $threewlid );
+	update_option( 'threewl_php_page_slug', $slug );
+
+	$post_title = $title;
+	$post_content = '<!-- threewl_php_page -->';
+	$post_status = 'publish';
+	$post_author = 1;
+	$post_name = $slug;
+	$post_type = 'page';
+
+	$post_data = compact( 'post_title', 'post_content', 'post_status',
+		              'post_author', 'post_name', 'post_type' );
+
+	$postID = wp_insert_post( $post_data );
+
+	if( !$postID ){
+	    echo '3WL Links Page page could not be created';
+	} else {
+	    echo '3WL Links Page page (ID ' . $postID . ') was created';
+	}
+
+	echo '</strong></p></div>';
+    } else if( isset( $_POST[ 'info_update' ] ) ){
+
+        echo '<div id="message" class="updated fade"><p><strong>';
+
+	update_option( 'threewl_php_page_threewlid', stripslashes( (string) $_POST['threewl_php_page_threewlid' ] ));
+	update_option( 'threewl_php_page_title', stripslashes( (string) $_POST['threewl_php_page_title' ] ));
+	update_option( 'threewl_php_page_slug', stripslashes( (string) $_POST['threewl_php_page_slug' ] ));
+	update_option( 'threewl_php_page_pp_help', (bool) $_POST['threewl_php_page_pp_help'] );
+	update_option( 'threewl_php_page_credit', (bool) $_POST['threewl_php_page_credit'] );
+
+	echo 'Configuration updated!';
+	echo '</strong></p></div>';
+    }
+
+    ?>
+
+    <div class="wrap">
+    <h2>3WL Links Page <?php echo $threewl_php_page_ver; ?></h2>
+    <p>The <a href="http://www.seo-traffic-guide.de/3WL-PHP-Page-Plugin/">3WL Links Page Plugin for WordPress</a> automatically generates the 3waylinks 
+    links page for the 3waylinks linking system by Jonathan Leger (<a href="http://www.seo-traffic-guide.de/recommends/3waylinks">3WayLinks.net</a>)
+    Please note that you have to be a paying member in the 3waylinks system in order to be able use this plugin, although the plugin itself comes free of charge.
+    </p>
+
+    <p>To use the plugin, insert the trigger text <strong>&lt;!--&nbsp;threewl_php_page&nbsp;--&gt;</strong> into an existing page. The trigger will be
+    automatically replaced with a complete 3waylinks link page.</p>
+
+    <p>For your convenience, the plugin can also create a new links page
+    for you. Simply fill in the title and slug (path) details and press
+    the "Create Page" button to create the 3WL links page. The trigger text
+    will be added automatically to the new page.</p>
+    <p style="color:red"><b>If you let the 3WL Links Page Plugin create your site, make sure that you have entered your 3Waylinks ID! Otherwise the page created will not work properly and you might get the warning "bad request" when you try to view the page</b></p>
+
+    <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+    <input type="hidden" name="info_update" id="info_update" value="true" />
+
+    <fieldset class="options">
+    <legend>3Waylinks-ID</legend>
+
+    <table width="100%" border="0" cellspacing="0" cellpadding="6">
+
+    <tr valign="top">
+      <td align="left" valign="middle">Your 3Waylinks Site ID 
+         <input name="threewl_php_page_threewlid" type="text" size="20" 
+                value="<?php echo htmlspecialchars( get_option( 'threewl_php_page_threewlid' ) ); ?>" />
+      </td>
+    </tr>
+
+    </table>
+
+    </fieldset>
+
+    <fieldset class="options">
+    <legend>Options</legend>
+
+    <ul>
+
+    <li><label for="threewl_php_page_credit">
+      <input type="checkbox" name="threewl_php_page_credit"
+             id="threewl_php_page_credit"
+             <?php echo ( get_option( 'threewl_php_page_credit' ) == true ? "checked=\"checked\"" : "" ) ?> />
+	     Include credit link for the 3WL Links Page Plugin for WordPress (thank you!)
+    </label></li>
+
+    </ul>
+
+    </fieldset>
+
+    <div class="submit">
+      <input type="submit" name="set_defaults" value="<?php _e('Load Default Options'); ?> &raquo;" />
+      <input type="submit" name="info_update" value="<?php _e('Update options' ); ?> &raquo;" />
+    </div>
+
+    <fieldset class="options">
+    <legend>Page Creation</legend>
+
+    <table width="100%" border="0" cellspacing="0" cellpadding="6">
+
+    <tr valign="top">
+      <td align="right" valign="middle"><strong>Page Title</strong></td>
+      <td align="left" valign="middle">
+         <input name="threewl_php_page_title" type="text" size="40" 
+                value="<?php echo htmlspecialchars( get_option( 'threewl_php_page_title' ) ); ?>" />
+      </td>
+    </tr>
+
+    <tr valign="top">
+      <td align="right" valign="middle"><strong>Page Slug</strong></td>
+      <td align="left" valign="middle">
+         <input name="threewl_php_page_slug" type="text" size="40" 
+                value="<?php echo htmlspecialchars( get_option( 'threewl_php_page_slug' ) ); ?>" />
+      </td>
+    </tr>
+
+    </table>
+
+    </fieldset>
+
+    <div class="submit">
+      <input type="submit" name="create_page" value="Create Page" />
+    </div>
+
+    </form>
+    
+    </div><?php
+}
+
+function threewl_php_page_process($content) {
+
+    $tag = "<!-- threewl_php_page -->";
+	
+    // Quickly leave if nothing to replace
+    
+    if( strpos( $content, $tag ) == false ) return $content;
+
+    // Otherwise generate the 3WL links PHP3WL links PHP3WL links PHP and sub it in
+
+    return str_replace( $tag, threewl_php_page_html(), $content );
+}
+
+function threewl_php_page_html(){
+    $sitename = get_option( 'threewl_php_page_sitename' );
+    $link_pp_help = get_option( 'threewl_php_page_pp_help' );
+    $link_credit = get_option( 'threewl_php_page_credit' );
+    $threewlid = get_option('threewl_php_page_threewlid');
+
+ 
+		if(!isset($_GET["article"])){
+  		$_GET["article"] = "";
+		}
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HTTPGET, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_ENCODING, "gzip,deflate");
+		curl_setopt($ch, CURLOPT_URL, "http://3waylinks.net/links.php?id=$threewlid&incode=1&article=$_GET[article]&pagevar=$_GET[nu]&g=" . urlencode(serialize($_GET)));
+		$pp = curl_exec($ch);
+		curl_close($ch);
+ 
+    if( $link_credit ){
+        $pp .= '<br><br><p style="color:grey;font-size:8px">This links page was generated with help of www.seo-traffic-guide.de - '
+	    . '<a target="_blank" href="http://www.seo-traffic-guide.de/">Find Do-Follow Blogs</a>.</p>'
+	    . "\n";
+    }
+
+    return $pp;
+}
+
+add_filter('the_content', 'threewl_php_page_process');
+add_action('admin_menu', 'threewl_php_page_options_setup');
+
+?>
